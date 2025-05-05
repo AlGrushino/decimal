ad9548b29045c83a3e3af1b244a0c8260f31427e
Alex <alex.grushinsky@mail.ru>
2025-05-05 13:28:28 +0300

    научился писать норм документацию на си, добавил шаблон для документации, вынес в отдельный хедер дефайны, написал несколько функций для работы с битами

--- src/bit_operations/bit_operations.c
+++ src/bit_operations/bit_operations.c
@@ -1,8 +1,9 @@
+#include "../something.h"
 
 /*
 Сдвиг работает таким образом, что зануляются все биты слева и справа от индекса
 */
-int s21_check_bit_set(int num, int index){
+int s21_check_bit(int num, int index){
     return num & (1 << index);
 }
 
@@ -18,4 +19,41 @@ bits = 1111 1110
 */
 int s21_unset_bit(int num, int index){
     return num & (~(1 << index));
+}
+
+/*
+@brief Проверяет, включён ли бит по index в decimal
+
+@param decimal число s21_decimal, которое проверяем
+@param index идндекс бита, который провеярем
+@return результат проверки, 1 - включён, 0 - выключен
+*/
+int s21_decimal_check_bit(s21_decimal decimal, int index){
+    int res = s21_check_bit(decimal.bits[index / MAX_BITS], index % MAX_BITS);
+    return res;
+}
+
+/*
+@brief Включает бит s21_decimal
+
+@param decimal число s21_decimal, в котором включаем бит
+@param index int - индекс бита, который включаем
+@return s21_decimal число decimal с включённым битом
+*/
+s21_decimal s21_decimal_set_bit(s21_decimal decimal, int index){
+    decimal.bits[index / MAX_BITS] = s21_set_bit(decimal.bits[index / MAX_BITS], index % MAX_BITS);
+    return decimal;
+}
+// Надо подумать, что возвращать из сет/ансет
+
+/*
+@brief Выключает бит s21_decimal
+
+@param decimal число s21_decimal, в котором выключаем бит
+@param index int индекс бита, который выключаем
+@return s21_decimal число decimal с выключенным битом
+*/
+s21_decimal s21_decimal_unset_bit(s21_decimal decimal, int index){
+    decimal.bits[index/MAX_BITS] = s21_unset_bit(decimal.bits[index / MAX_BITS], index % MAX_BITS);
+    return decimal;
 }
