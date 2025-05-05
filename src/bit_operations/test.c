264a406d3e0af68af1f279d878ce28f443061169
Alex <alex.grushinsky@mail.ru>
2025-05-05 20:46:00 +0300

    добавил тест на последний бит

--- src/bit_operations/test.c
+++ src/bit_operations/test.c
@@ -524,6 +524,31 @@ START_TEST(s21_decimal_set_bit_4) {
 }
 END_TEST
 
+// третий бит
+START_TEST(s21_decimal_set_bit_5) {
+  s21_decimal num = {{0, 0, 0, 0}};
+  int diff = 64;
+  num = s21_decimal_set_bit(num, diff + 0);
+
+  ck_assert_int_eq(num.bits[2], 1);
+}
+END_TEST
+
+START_TEST(s21_decimal_set_bit_6) {
+  int res = 2;
+  int bit = 1;
+  int diff = 64;
+
+  for (size_t i = 1; i < 8; i++) {
+    s21_decimal num = {{0, 0, 0, 0}};
+    num = s21_decimal_set_bit(num, diff + bit);
+    ck_assert_int_eq(num.bits[2], res);
+
+    bit++;
+    res *= 2;
+  }
+}
+END_TEST
 // бит включён
 
 int main(void) {
@@ -598,6 +623,9 @@ int main(void) {
   tcase_add_test(tc1_1, s21_decimal_set_bit_2);
   tcase_add_test(tc1_1, s21_decimal_set_bit_3);
   tcase_add_test(tc1_1, s21_decimal_set_bit_4);
+  tcase_add_test(tc1_1, s21_decimal_set_bit_5);
+  tcase_add_test(tc1_1, s21_decimal_set_bit_6);
+
 
   srunner_set_fork_status(sr, CK_NOFORK);
   srunner_run_all(sr, CK_ENV);
