370a1d614654b3a2b217cd1998d9c56fd99c098b
a;yonanaaa <alyonka20042004@gmail.com>
2025-06-05 21:21:18 +0300

    add s21 to every function

--- src/main.c
+++ src/main.c
@@ -1,14 +1,14 @@
 #include "s21_decimal.h"
 #include <limits.h>
 
-void print_binary(unsigned int num) {
+void s21_print_binary(unsigned int num) {
   for (int i = 31; i >= 0; i--) {
     printf("%d", (num >> i) & 1);
   }
   printf(" ");
 }
 
-void mult_by_ten(s21_decimal *res) { // <<1 умножение на два: (<<1 + <<3) == умножение на 10
+void s21_mult_by_ten(s21_decimal *res) { // <<1 умножение на два: (<<1 + <<3) == умножение на 10
   unsigned int temp_mantissa[3] = {res->bits[0], res->bits[1], res->bits[2]}; //92 бита
   unsigned long long temp = 0; //64 бита (больше 32 на случай, если будет overflow)
   unsigned long long overflow = 0;
@@ -33,13 +33,13 @@ void mult_by_ten(s21_decimal *res) { // <<1 умножение на два: (<<1
   }
 }
 
-void mult_by_ten_times(s21_decimal *res, int times) {
+void s21_mult_by_ten_times(s21_decimal *res, int times) {
   for (int i = 0; i<times; i++) {
-      mult_by_ten(res);
+    s21_mult_by_ten(res);
   }
 }
 
-void normal_rounding(unsigned int mantissa[3], unsigned long long rest) {
+void s21_normal_rounding(unsigned int mantissa[3], unsigned long long rest) {
   if (rest >= 5) {
       int carry = 1;
       for (int i = 0; i < 3 && carry; i++) {
@@ -50,7 +50,7 @@ void normal_rounding(unsigned int mantissa[3], unsigned long long rest) {
   }
 }
 
-void div_by_ten(s21_decimal *res) { //??????
+void s21_div_by_ten(s21_decimal *res) { //??????
   unsigned int temp_mantissa[3] = {res->bits[0], res->bits[1], res->bits[2]}; //92 бита
   unsigned long long temp = 0; //64 бита (больше 32 на случай, если будет overflow) 
   unsigned long long rest = 0;
@@ -59,13 +59,13 @@ void div_by_ten(s21_decimal *res) { //??????
       temp_mantissa[i] = (unsigned int)(temp / 10);
       rest = temp % 10;
   }
-  normal_rounding(temp_mantissa, rest);
+  s21_normal_rounding(temp_mantissa, rest);
   for (int i = 0; i<3;i++) {
       res->bits[i] = temp_mantissa[i];
   }
 }
 
-void div_by_ten_without_rounding(s21_decimal *res) { //??????
+void s21_div_by_ten_without_rounding(s21_decimal *res) { //??????
   unsigned int temp_mantissa[3] = {res->bits[0], res->bits[1], res->bits[2]}; //92 бита
   unsigned long long temp = 0; //64 бита (больше 32 на случай, если будет overflow) 
   unsigned long long rest = 0;
@@ -76,28 +76,28 @@ void div_by_ten_without_rounding(s21_decimal *res) { //??????
   }
 }
 
-void div_by_ten_times(s21_decimal *res, int times) {
+void s21_div_by_ten_times(s21_decimal *res, int times) {
   for (int i = 0; i<times; i++) {
-      div_by_ten(res);
+    s21_div_by_ten(res);
   }
 }
 
-void set_scale(s21_decimal *res, unsigned char scale) { //unsigned пч нет отриц, char потому что он как раз 8 бит 
+void s21_set_scale(s21_decimal *res, unsigned char scale) { //unsigned пч нет отриц, char потому что он как раз 8 бит 
   res->bits[3] &= ~(0xFF << 16);
   res->bits[3] |= ((int)scale << 16); // приводим к int так как работаем со структурой интов 
 }
 
-int get_scale(s21_decimal *res) {
+int s21_get_scale(s21_decimal *res) {
   return (res->bits[3] >> 16) & 0xFF;
 }
 
-void get_mantissa(const s21_decimal *dec, unsigned int mantissa[3]) {
+void s21_get_mantissa(const s21_decimal *dec, unsigned int mantissa[3]) {
   mantissa[0] = dec->bits[0];
   mantissa[1] = dec->bits[1];
   mantissa[2] = dec->bits[2];
 }
 
-int is_divisible_by_10(s21_decimal *dec) {
+int s21_is_divisible_by_10(s21_decimal *dec) {
   unsigned int temp_mantissa[3] = {dec->bits[0], dec->bits[1], dec->bits[2]};
   unsigned long long temp = 0;
   unsigned long long rest = 0;
@@ -112,50 +112,50 @@ int is_divisible_by_10(s21_decimal *dec) {
 }
 
 
-void normalization(s21_decimal *value1, s21_decimal *value2) { // do: round?
-  int value1_scale = get_scale(value1);
-  int value2_scale = get_scale(value2);
+void s21_normalization(s21_decimal *value1, s21_decimal *value2) { // do: round?
+  int value1_scale = s21_get_scale(value1);
+  int value2_scale = s21_get_scale(value2);
   if (value1_scale <= 28 && value2_scale <= 28) {
       if (value1_scale > value2_scale) {
           int diff = value1_scale - value2_scale;
-          set_scale(value2, value1_scale);
-          mult_by_ten_times(value2, diff);
+          s21_set_scale(value2, value1_scale);
+          s21_mult_by_ten_times(value2, diff);
       }
       else {
           int diff = value2_scale - value1_scale;
-          set_scale(value1, value2_scale);
-          mult_by_ten_times(value1, diff);
+          s21_set_scale(value1, value2_scale);
+          s21_mult_by_ten_times(value1, diff);
       }
   } else if (value1_scale > 28 || value2_scale > 28) {
       int diff_val1 = 28 - value1_scale;
       int diff_val2 = 28 - value2_scale;
       if (diff_val1 > 0) {
-          mult_by_ten_times(value1, diff_val1);
+        s21_mult_by_ten_times(value1, diff_val1);
       }
       else if (diff_val1 < 0) {
-          div_by_ten_times(value1, -diff_val1);
+        s21_div_by_ten_times(value1, -diff_val1);
       }
-      set_scale(value1, 28);
+      s21_set_scale(value1, 28);
       if (diff_val2 > 0) {
-          mult_by_ten_times(value2, diff_val2);
+        s21_mult_by_ten_times(value2, diff_val2);
       }
       else if (diff_val2 < 0) {
-          div_by_ten_times(value2, -diff_val2);
+        s21_div_by_ten_times(value2, -diff_val2);
       }
-      set_scale(value2, 28);
+      s21_set_scale(value2, 28);
   }
-  int new_scale = get_scale(value1);
+  int new_scale = s21_get_scale(value1);
   printf("new_scale:%d\n", new_scale);
-  while (new_scale > 0 && is_divisible_by_10(value1) && is_divisible_by_10(value2)) { //сокрщаем 
-      div_by_ten_without_rounding(value1);
-      div_by_ten_without_rounding(value2);
+  while (new_scale > 0 && s21_is_divisible_by_10(value1) && s21_is_divisible_by_10(value2)) { //сокрщаем 
+      s21_div_by_ten_without_rounding(value1);
+      s21_div_by_ten_without_rounding(value2);
       new_scale--;
-      set_scale(value1, new_scale);
-      set_scale(value2, new_scale);
+      s21_set_scale(value1, new_scale);
+      s21_set_scale(value2, new_scale);
   }
 
 }
-int is_less_bits(s21_decimal value1, s21_decimal value2) {
+int s21_is_less_bits(s21_decimal value1, s21_decimal value2) {
   int flag = 0;
   for (int i = 2; i>=0; i--) {
       if (value1.bits[i] < value2.bits[i]){
@@ -165,11 +165,11 @@ int is_less_bits(s21_decimal value1, s21_decimal value2) {
   return flag;
 }
 
-int get_sign(s21_decimal *res) {
+int s21_get_sign(s21_decimal *res) {
   return (res->bits[3] >> 31) & 1;
 }
 
-void set_sign(s21_decimal *res, int sign) { //а где проверять, чтобы знак был норм? 
+void s21_set_sign(s21_decimal *res, int sign) { //а где проверять, чтобы знак был норм? 
   unsigned int mask_for_last_bit = 1<<31;
   if (sign) {
       res->bits[3] |= mask_for_last_bit;
@@ -180,18 +180,18 @@ void set_sign(s21_decimal *res, int sign) { //а где проверять, чт
 
 int s21_is_less(s21_decimal value1, s21_decimal value2) { //если оба нули?
   int flag = 0;
-  int sign1 = get_sign(&value1);  
-  int sign2 = get_sign(&value2);
-  normalization(&value1, &value2);
+  int sign1 = s21_get_sign(&value1);  
+  int sign2 = s21_get_sign(&value2);
+  s21_normalization(&value1, &value2);
 
   if (sign1 > sign2) {
       flag = 1;
   } else if (sign1 == sign2) {
       if (sign1 == 1) {
-          flag = is_less_bits(value2, value1);
+          flag = s21_is_less_bits(value2, value1);
       }
       else {
-          flag = is_less_bits(value1, value2);
+          flag = s21_is_less_bits(value1, value2);
       }
   }
   return flag;
@@ -199,7 +199,7 @@ int s21_is_less(s21_decimal value1, s21_decimal value2) { //если оба ну
 
 int s21_is_equal(s21_decimal value1, s21_decimal value2) {
   int flag = 1;
-  normalization(&value1, &value2);
+  s21_normalization(&value1, &value2);
   for (int i = 0; i<4 && flag; i++) {
       if (value1.bits[i] != value2.bits[i]) {
           flag = 0;
@@ -246,24 +246,24 @@ int main ()
   printf("\n");
   s21_decimal num1 = {{1500, 0, 0, 0}};
   s21_decimal num2 = {{1500, 0, 0, 0}};
-  set_scale(&num1, 30);
-  set_scale(&num2, 30);
+  s21_set_scale(&num1, 30);
+  s21_set_scale(&num2, 30);
   //div_by_ten_times(&num1, 1);
   //set_sign(&num1, 1);
   //set_sign(&num2, 1);
   //int res = s21_is_equal(num1, num2);
 
   //div_by_ten(&num);
-  normalization(&num1, &num2);
-  print_binary(num1.bits[3]);
-  print_binary(num1.bits[2]);
-  print_binary(num1.bits[1]);
-  print_binary(num1.bits[0]);
+  s21_normalization(&num1, &num2);
+  s21_print_binary(num1.bits[3]);
+  s21_print_binary(num1.bits[2]);
+  s21_print_binary(num1.bits[1]);
+  s21_print_binary(num1.bits[0]);
   printf("\n");
-  print_binary(num2.bits[3]);
-  print_binary(num2.bits[2]);
-  print_binary(num2.bits[1]);
-  print_binary(num2.bits[0]);
+  s21_print_binary(num2.bits[3]);
+  s21_print_binary(num2.bits[2]);
+  s21_print_binary(num2.bits[1]);
+  s21_print_binary(num2.bits[0]);
   printf("\n");
 
 
--- src/other/s21_other.h
+++ src/other/s21_other.h
@@ -10,21 +10,21 @@ int s21_negate(s21_decimal value, s21_decimal *result);
 
 
 //my
-void mult_by_ten(s21_decimal *res);
-void mult_by_ten_times(s21_decimal *res, int times);
-void normal_rounding(unsigned int mantissa[3], unsigned long long rest);
-void div_by_ten(s21_decimal *res);
-void div_by_ten_times(s21_decimal *res, int times);
-void set_scale(s21_decimal *res, unsigned char scale);
-int get_scale(s21_decimal *res);
-int is_divisible_by_10(s21_decimal *dec);
-void div_by_ten_without_rounding(s21_decimal *res);
-void normalization(s21_decimal *value1, s21_decimal *value2);
-int get_sign(s21_decimal *res);
-void set_sign(s21_decimal *res, int sign);
+void s21_mult_by_ten(s21_decimal *res);
+void s21_mult_by_ten_times(s21_decimal *res, int times);
+void s21_normal_rounding(unsigned int mantissa[3], unsigned long long rest);
+void s21_div_by_ten(s21_decimal *res);
+void s21_div_by_ten_times(s21_decimal *res, int times);
+void s21_set_scale(s21_decimal *res, unsigned char scale);
+int s21_get_scale(s21_decimal *res);
+int s21_is_divisible_by_10(s21_decimal *dec);
+void s21_div_by_ten_without_rounding(s21_decimal *res);
+void s21_normalization(s21_decimal *value1, s21_decimal *value2);
+int s21_get_sign(s21_decimal *res);
+void s21_set_sign(s21_decimal *res, int sign);
 int s21_is_equal(s21_decimal value1, s21_decimal value2);
 int s21_is_not_equal(s21_decimal value1, s21_decimal value2);
-int is_less_bits(s21_decimal value1, s21_decimal value2);
+int s21_is_less_bits(s21_decimal value1, s21_decimal value2);
 int s21_is_less(s21_decimal value1, s21_decimal value2);
 int s21_is_less_or_equal(s21_decimal value1, s21_decimal value2);
 int s21_is_greater(s21_decimal value1, s21_decimal value2);
